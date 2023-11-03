32d7a42de6036c2eaca05a20c2a2a794dd48c1e3
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-11-03 12:01:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let panels=document.querySelectorAll(".panel");
+
+for(let i=0;i<panels.length;i++){
+	panels[i].addEventListener("click", onClickToggle)
+}
+
+function onClickToggle(e) {
+let current=e.target;
+	
+	for(let i=0;i<panels.length;i++){
+	if(panels.[i].classList.containe(".active")){
+		panels.[i].classList.toggle(".active")
+	}
+		current.classList.toggle(".active");
+}
+}
