db2e34d29c2637ea0a560620904894e7318944a9
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-11-03 12:32:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 //your JS code here. If required.
 let panels=document.querySelectorAll(".panel");
 
-for(let i=0;i<panels.length;i++){
-	panels[i].addEventListener("click", onClickToggle)
-}
+// for(let i=0;i<panels.length;i++){
+// 	panels[i].addEventListener("click", onClickToggle)
+// }
 
 function onClickToggle(e) {
-let current=e.target;
+let current=e.id;
+	
 	console.log(current);
-	// for(let i=0;i<panels.length;i++){
-	// if(panels[i].classList.contains("active")){
-	// 	panels[i].classList.remove("active")
-	// }
-		if(current.classList.contains("active")){
-			return;
-		}
-		else{
-			current.classList.add("active")
-		}
+	for(let i=0;i<panels.length;i++){
+	
+		panels[i].classList.remove("active")
+	
+	}
+		
+		
+			e.classList.add("active");
 			
 			
 }
